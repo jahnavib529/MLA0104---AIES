95f3f2f69f94d3a304d42217f9a27c5c45690814
jahnavib529 <jahnavibandaru529@gmail.com>
2026-08-04 14:30:50 +0530

    Add files via upload

---
+++ Prolog/Database with Name and DOB/namedob.pl
@@ -0,0 +1,7 @@
+% Database: Name and Date of Birth
+
+person(john, '12-05-2000').
+person(anil, '25-08-1999').
+person(priya, '10-01-2001').
+person(rahul, '15-07-1998').
+person(sneha, '30-12-2002').
