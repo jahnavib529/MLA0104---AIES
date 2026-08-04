f9ad5c7bdaaf23ec8ef152602760b200c7407258
jahnavib529 <jahnavibandaru529@gmail.com>
2026-08-04 14:41:18 +0530

    Add files via upload

---
+++ Prolog/Planets Database/planets.pl
@@ -0,0 +1,8 @@
+planet(mercury, terrestrial, 88).
+planet(venus, terrestrial, 225).
+planet(earth, terrestrial, 365).
+planet(mars, terrestrial, 687).
+planet(jupiter, gas_giant, 4333).
+planet(saturn, gas_giant, 10759).
+planet(uranus, ice_giant, 30687).
+planet(neptune, ice_giant, 60190).
